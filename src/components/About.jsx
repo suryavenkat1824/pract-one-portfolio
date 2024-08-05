2347ff7aa751df6bdd36b7b7380477f1194d1cfa
Kesarapu Surya Venkat <78261318+suryavenkat1824@users.noreply.github.com>
2024-08-05 17:04:38 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,16 +10,12 @@ const About = () => {
                 About</p>
             </div>
             <p class="text-xl mt-20">
-           I'm Kesarapu Surya Venkat, a web developer and competitive programming enthusiast pursuing 
-             Bachelor of Technology in Computer Science and Engineering at NIT Silchar.
-              I have a passion for creating visually appealing interfaces and excelling in data structures and algorithms.
-               I'm committed to continuous learning and aspire to make a lasting impact in the tech industry by delivering innovative 
-              solutions and exceptional user experiences.
+            I'm Kesarapu Surya Venkat, a web developer and competitive programming enthusiast who graduated with a Bachelor of Technology in Computer Science and Engineering from NIT Silchar. I have a passion for creating visually appealing interfaces and excelling in data structures and algorithms. I'm committed to continuous learning and aspire to make a lasting impact in the tech industry by delivering innovative solutions and exceptional user experiences.
             </p>
             <br>
             </br>
             <p class="text-xl">
-        With a strong drive for growth, I am focused on refining my backend skills and coding abilities. 
+            With a strong drive for growth, I am focused on refining my backend skills and coding abilities. 
             I am particularly interested in data handling, server-side technologies, and databases
              to develop robust web solutions. My passion for competitive programming motivates me to solve 
              complex challenges and contribute cutting-edge solutions. Each skill acquired brings me closer to
